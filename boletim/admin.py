a74be144fc977303c585b3d1d5bf92e4482dd57e
Diego Eduardo Ferreira Biavati <Diego>
2021-01-22 17:10:04 -0300

    Adicionado o tipo de BI

--- boletim/admin.py
+++ boletim/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Boletim
+from .models import Boletim, TipoBI
 
 # Register your models here.
-admin.site.register(Boletim)
+@admin.register(Boletim)
+class BoletimAdmin(admin.ModelAdmin):
+    date_hierarchy = 'dataPublicacao'
+
+@admin.register(TipoBI)
+class TipoBIAdmin(admin.ModelAdmin):
+    pass
--- boletim/models.py
+++ boletim/models.py
@@ -1,15 +1,29 @@
+'''
+Importa o módulo de banco de dados padrão do Django
+'''
 from django.db import models
 
-# Create your models here.
+class TipoBI(models.Model):
+    descricao = models.CharField(max_length=250)
+    abreviatura = models.CharField(max_length=250)
+    slug = models.SlugField()
+    numeroUltimaPagina = models.IntegerField()
+    numeroUltimoBI = models.IntegerField()
+    iniciaNumeroPagina = models.IntegerField()
+    eAditamento = models.BooleanField()
+    imprimeBordas = models.BooleanField()
+    titulo = models.CharField(max_length=250)
+
 class Boletim(models.Model):
-    pagInicial = models.IntegerField()
-    pagFinal = models.IntegerField()
+    ''' Classe Boletim '''
+    paginaInicial = models.IntegerField()
+    paginaFinal = models.IntegerField()
     aprovado = models.BooleanField(default=False)
     assinado = models.BooleanField(default=False)
     numeroBI = models.IntegerField()
     assinaConfereBI = models.BooleanField(default=False)
-    dataPub = models.DateField()
+    dataPublicacao = models.DateField()
     slug = models.SlugField()
-    #tipoPub = models.
+    tipoPub = models.ForeignKey(TipoBI, on_delete=models.CASCADE)
     #biRef = models.
     #colMateriaBI = models.
